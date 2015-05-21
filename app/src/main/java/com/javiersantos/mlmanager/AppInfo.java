a28707d38207ba0e8b591b3b3b1cb9113473b995
Javier Santos <javier.santos.vel@gmail.com>
2015-05-21 17:36:09 +0200

    AppInfo variables public instead of protected

--- app/src/main/java/com/javiersantos/mlmanager/AppInfo.java
+++ app/src/main/java/com/javiersantos/mlmanager/AppInfo.java
@@ -3,10 +3,10 @@ package com.javiersantos.mlmanager;
 import android.graphics.drawable.Drawable;
 
 public class AppInfo {
-    protected String name;
-    protected String apk;
-    protected String source;
-    protected String data;
-    protected Drawable icon;
+    public String name;
+    public String apk;
+    public String source;
+    public String data;
+    public Drawable icon;
 
 }
