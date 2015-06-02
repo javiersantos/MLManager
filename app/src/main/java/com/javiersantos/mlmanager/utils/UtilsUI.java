941c1670254b249e22f45395b1e61073bc2c32f7
Javier Santos <javier.santos.vel@gmail.com>
2015-06-02 14:57:49 +0200

    Delete unnecessary import

--- app/src/main/java/com/javiersantos/mlmanager/utils/UtilsUI.java
+++ app/src/main/java/com/javiersantos/mlmanager/utils/UtilsUI.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
