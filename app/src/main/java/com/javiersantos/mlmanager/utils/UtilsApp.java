8594fc510cc241e3ff08964f5c32a4cb08bf35d3
Javier Santos <javier.santos.vel@gmail.com>
2015-06-02 14:19:41 +0200

    Add UtilsUI

--- app/src/main/java/com/javiersantos/mlmanager/utils/UtilsApp.java
+++ app/src/main/java/com/javiersantos/mlmanager/utils/UtilsApp.java
@@ -70,13 +70,4 @@ public class UtilsApp {
         return res;
     }
 
-    public static int darker (int color, double factor) {
-        int a = Color.alpha( color );
-        int r = Color.red(color);
-        int g = Color.green( color );
-        int b = Color.blue(color);
-
-        return Color.argb(a, Math.max( (int)(r * factor), 0 ), Math.max( (int)(g * factor), 0 ), Math.max( (int)(b * factor), 0 ) );
-    }
-
 }
---
+++ app/src/main/java/com/javiersantos/mlmanager/utils/UtilsUI.java
@@ -0,0 +1,96 @@
+package com.javiersantos.mlmanager.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.javiersantos.mlmanager.AboutActivity;
+import com.javiersantos.mlmanager.MainActivity;
+import com.javiersantos.mlmanager.R;
+import com.javiersantos.mlmanager.SettingsActivity;
+import com.mikepenz.iconics.typeface.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
+import java.util.Calendar;
+
+public class UtilsUI {
+
+    public static int darker (int color, double factor) {
+        int a = Color.alpha(color);
+        int r = Color.red(color);
+        int g = Color.green( color );
+        int b = Color.blue(color);
+
+        return Color.argb(a, Math.max( (int)(r * factor), 0 ), Math.max( (int)(g * factor), 0 ), Math.max( (int)(b * factor), 0 ) );
+    }
+
+    public static Drawer setNavigationDrawer (Activity activity, final Context context, Toolbar toolbar) {
+        int header;
+        if (getDayOrNight() == 1) {
+            header = R.drawable.header_day;
+        } else {
+            header = R.drawable.header_night;
+        }
+
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(activity)
+                .withHeaderBackground(header)
+                .build();
+
+        return new DrawerBuilder()
+                .withActivity(activity)
+                .withToolbar(toolbar)
+                .withAccountHeader(headerResult)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(context.getResources().getString(R.string.app_name)).withIcon(ContextCompat.getDrawable(context, R.mipmap.ic_launcher)),
+                        new DividerDrawerItem(),
+                        new SecondaryDrawerItem().withName(context.getResources().getString(R.string.action_settings)).withIcon(FontAwesome.Icon.faw_cog),
+                        new SecondaryDrawerItem().withName(context.getResources().getString(R.string.action_about)).withIcon(FontAwesome.Icon.faw_info)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
+                        switch (position) {
+                            case 1:
+                                context.startActivity(new Intent(context, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                                break;
+                            case 2:
+                                context.startActivity(new Intent(context, SettingsActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                                break;
+                            case 3:
+                                context.startActivity(new Intent(context, AboutActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                                break;
+                            default:
+                                break;
+                        }
+
+                        return false;
+                    }
+                }).build();
+
+    }
+
+    public static int getDayOrNight() {
+        int actualHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+
+        if (actualHour >= 8 && actualHour < 19) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+}
