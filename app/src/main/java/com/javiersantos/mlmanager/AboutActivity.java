e40b4bb1f3480f40563b0260ea816b5b652c4c34
Javier Santos <javier.santos.vel@gmail.com>
2015-06-02 15:22:36 +0200

    Add "Rate on Google Play" on About

--- app/src/main/java/com/javiersantos/mlmanager/AboutActivity.java
+++ app/src/main/java/com/javiersantos/mlmanager/AboutActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.javiersantos.mlmanager.utils.AppPreferences;
+import com.javiersantos.mlmanager.utils.UtilsApp;
 import com.javiersantos.mlmanager.utils.UtilsUI;
 
 public class AboutActivity extends AppCompatActivity {
@@ -53,10 +54,13 @@ public class AboutActivity extends AppCompatActivity {
 
     private void setScreenElements() {
         TextView header = (TextView) findViewById(R.id.header);
+        TextView appNameVersion = (TextView) findViewById(R.id.app_name);
         CardView about_1 = (CardView) findViewById(R.id.about_1);
         CardView about_2 = (CardView) findViewById(R.id.about_2);
+        CardView about_googleplay = (CardView) findViewById(R.id.about_googleplay);
 
         header.setBackgroundColor(appPreferences.getPrimaryColorPref());
+        appNameVersion.setText(getResources().getString(R.string.app_name) + " Beta v" + UtilsApp.getAppVersionName(getApplicationContext()));
         about_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -67,6 +71,12 @@ public class AboutActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://plus.google.com/+javitoro95/")));
+            }
+        });
+        about_googleplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(UtilsApp.goToGooglePlay("com.javiersantos.mlmanager"));
             }
         });
     }
