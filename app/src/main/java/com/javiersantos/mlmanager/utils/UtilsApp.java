5e4fcec283f64aa843b9fd1ae12a7e4c388cf015
Javier Santos <javier.santos.vel@gmail.com>
2015-06-07 22:08:28 +0200

    Add getShareIntent method to UtilsApp

--- app/src/main/java/com/javiersantos/mlmanager/utils/UtilsApp.java
+++ app/src/main/java/com/javiersantos/mlmanager/utils/UtilsApp.java
@@ -122,4 +122,14 @@ public class UtilsApp {
         return res;
     }
 
+    public static Intent getShareIntent(File file) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+        intent.setType("application/vnd.android.package-archive");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        return intent;
+    }
+
 }
