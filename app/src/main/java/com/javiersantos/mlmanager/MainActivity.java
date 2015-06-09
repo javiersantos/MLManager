3883c91e80d48cb80f581215872c530105919911
Javier Santos <javier.santos.vel@gmail.com>
2015-06-09 17:20:45 +0200

    Remove unnecessary import

--- app/src/main/java/com/javiersantos/mlmanager/MainActivity.java
+++ app/src/main/java/com/javiersantos/mlmanager/MainActivity.java
@@ -2,7 +2,6 @@ package com.javiersantos.mlmanager;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
