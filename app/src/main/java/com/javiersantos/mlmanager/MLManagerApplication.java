a7363854e4fb67d38904f6e9d881efe194f7fed3
Javier Santos <me@javiersantos.me>
2016-03-29 15:05:52 +0200

    Fix #23 build param for pro

--- app/src/main/java/com/javiersantos/mlmanager/MLManagerApplication.java
+++ app/src/main/java/com/javiersantos/mlmanager/MLManagerApplication.java
@@ -3,15 +3,25 @@ package com.javiersantos.mlmanager;
 import android.app.Application;
 
 import com.javiersantos.mlmanager.utils.AppPreferences;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.Iconics;
 
 public class MLManagerApplication extends Application {
-
     private static AppPreferences sAppPreferences;
+    private static boolean isPro;
 
     @Override
     public void onCreate() {
-        sAppPreferences = new AppPreferences(this);
         super.onCreate();
+
+        // Load Shared Preference
+        sAppPreferences = new AppPreferences(this);
+
+        // Check if there is the Pro version
+        isPro = this.getPackageName().equals(getProPackage());
+
+        // Register custom fonts like this (or also provide a font definition file)
+        Iconics.registerFont(new GoogleMaterial());
     }
 
     public static AppPreferences getAppPreferences() {
@@ -23,7 +33,11 @@ public class MLManagerApplication extends Application {
      * @return true for ML Manager Pro, false otherwise
      */
     public static Boolean isPro() {
-        return false;
+        return isPro;
+    }
+
+    public static void setPro(Boolean res) {
+        isPro = res;
     }
 
     public static String getProPackage() {
